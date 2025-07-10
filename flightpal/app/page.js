670588b5a8d7dd3dd65772ca1d3346719487546e
Lucas <derusheluc@yahoo.com>
2025-07-09 19:58:59 -0500

    added introtext paragraph with css

--- flightpal/app/page.js
+++ flightpal/app/page.js
@@ -4,15 +4,23 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
 <main>
-    <div className={styles.introtext}>
-       <img src="favicon.ico" alt="App Logo" />
+         <img src="favicon.ico" alt="App Logo" />
       <nav>
         <a href="/page.js">Home</a>
         <a href="page.js"></a>
         <a href="page.js">Home</a>
         <a href="page.js">Home</a>
       </nav>
-      <h1>Your Travel Companion . All in an App.</h1>
+  <div className={styles.introtextcontainer}>
+    <div className={styles.mainintrotext}>
+      <h1>Your Travel Companion. All in an App.</h1>
+    </div>
+    <div className={styles.paragraphintrotext}>
+      <p>The only travel app that allows you to find flights, track your plane, give alerts about your flights, and log them—All in one practical and clean app.</p>
+    </div>
+  </div>
+    <div className={styles.introtext}>
+
     </div>
 </main>
   );
