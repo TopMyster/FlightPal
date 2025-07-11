0b2da38cfdad165217f5bb3fecb5cbd08cd5b0cc
Lucas <derusheluc@yahoo.com>
2025-07-10 21:26:45 -0500

    another one

--- flightpal/components/navbar.js
+++ flightpal/components/navbar.js
@@ -1,47 +1,71 @@
 'use client';
-
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import styles from './Navbar.module.css';
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
-  const [scrolled, setScrolled] = useState(false);
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
-    const onScroll = () => {
-      setScrolled(window.scrollY > 10);
-    };
-
-    window.addEventListener('scroll', onScroll);
-    return () => window.removeEventListener('scroll', onScroll);
+    const handleScroll = () => setIsScrolled(window.scrollY > 0);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <nav className={`${styles.navbar} ${scrolled ? styles.scrolled : ''}`}>
-      <div className={styles.navContent}>
-        {/* Logo */}
+    <header className={`${styles.navbar} ${isScrolled ? styles.scrolled : ""}`}>
+      <div className={`${isScrolled ? styles.scrolledContent : styles.navContent}`}>
+        {/* Left Logo/Icon */}
         <div className={styles.navLeft}>
-          <Image src="/favicon.ico" alt="FlightPal logo" width={150} height={40} />
-        </div>
-        {/* Desktop Nav */}
+  {isScrolled && (
+    <>
+      <Image
+        src="/flightpalwing.png"
+        alt="flightpal logo wing"
+        width={32}
+        height={32}
+        className={`${styles.iconLogo} ${styles.logoVisible}`}
+      />
+      <div className={styles.divider} />
+    </>
+  )}
+  {!isScrolled && (
+    <a href="#">
+    <Image
+      src="/favicon.ico"
+      alt="FlightPal Logo"
+      width={150}
+      height={40}
+      className={`${styles.logo} ${styles.logoVisible}`}
+    />
+    </a>
+  )}
+</div>
+
+
+        {/* Nav Links */}
         <div className={styles.navCenter}>
-          <Link href="/" className={styles.navbarlinks}>Home</Link>
-          <Link href="/about" className={styles.navbarlinks}>About</Link>
-          <Link href="/faq" className={styles.navbarlinks}>FAQ</Link>
-          <Link href="/support" className={styles.navbarlinks}>Support</Link>
+          <Link href="#">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/FAQ">FAQ</Link>
+          <Link href="#">Support</Link>
         </div>
 
-        {/* CTA */}
-        <div className={styles.navRight}>
-          <Link href="signup">
-            Get Started 
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 18" className={styles.icon}>
+        {/* Get Started */}
+        <div className={isScrolled ? styles.navRightScrolled : styles.navRight}>
+          <Link href="#">
+            Get Started
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 12 18"
+              width={16}
+              height={16}
+            >
               <path
                 d="M 5.25 3.75 L 6.75 3.75 M 3.75 16.5 L 8.25 16.5 C 9.493 16.5 10.5 15.493 10.5 14.25 L 10.5 3.75 C 10.5 2.507 9.493 1.5 8.25 1.5 L 3.75 1.5 C 2.507 1.5 1.5 2.507 1.5 3.75 L 1.5 14.25 C 1.5 15.493 2.507 16.5 3.75 16.5 Z"
                 fill="none"
-                stroke="black"
+                stroke={isScrolled ? "white" : "black"}
                 strokeWidth="1.5"
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -49,7 +73,7 @@ export default function Navbar() {
             </svg>
           </Link>
         </div>
-        </div>
-    </nav>
+      </div>
+    </header>
   );
 }
