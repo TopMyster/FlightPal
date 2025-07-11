5c6cc2566ebd8345a4e092bfe6bcd266d9dfe05d
Lucas <derusheluc@yahoo.com>
2025-07-10 21:32:31 -0500

    fixed page bc what did you do to it

--- flightpal/app/page.js
+++ flightpal/app/page.js
@@ -7,7 +7,6 @@ import Link from 'next/link';
 export default function Home() {
   return (
 <main className={styles.main}>
-  <center>
   <Navbar />
   <div className={styles.introtextcontainer}>
     <div className={styles.mainintrotext}>
@@ -17,15 +16,16 @@ export default function Home() {
       <p>The only travel app that allows you to find flights, track your plane, give alerts about your flights, and log them—All in one practical and clean app.</p>
     </div>
   </div>
-  <ul className={styles.featurelist}>
-    <h1>Features</h1>
-<li>•Stay updated with live flight status.</li>
-<li>•Find flights from top airlines in seconds.</li>
-<li>•Find hotels nearby to your flights destination.</li>
-<li>•Get notified about delays, cancellations, and boarding times.</li>
-<li></li>
- </ul>
-  </center>
+  
+  <div className={styles.showcaseContainer}>
+  <Image 
+  className={styles.showcase}
+  src={'/iPhoneholding.avif'}
+  width={532}
+  height={725}
+  alt="showcase"
+  />
+  </div>
 </main>
   );
 }
